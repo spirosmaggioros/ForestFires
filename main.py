1cd5fc379eb56507ba0f439ccda044e82235c525
Spiros Maggioros <spirosastro@gmail.com>
2023-07-30 15:51:58 +0300

    update 3d graph

--- main.py
+++ main.py
@@ -20,3 +20,19 @@ data = process_data_for_clustering(data)
 y_pred, y_test = KNN_algorithm(data)
 print(metrics.confusion_matrix(y_test, y_pred))
 print(metrics.accuracy_score(y_test, y_pred))
+
+colors = {1:'blue' , 2:'yellow' , 3:'red' , 4:'darkred'}
+
+fig = go.Figure(layout=go.Layout(showlegend=True,template=None,scene=dict(xaxis = dict(title='RH'),yaxis = dict(title='wind'),zaxis = dict(title='temp')),title=go.layout.Title(text="3D Cluster After Preprocessing for cluster: " , font=dict(family="Arial",size=20,color='#000000'))))
+fig.add_trace(
+    go.Scatter3d(
+    x = data['RH'],
+    y = data['wind'],
+    z = data['temp'],
+    mode='markers',
+    marker=dict(color=data['danger'].map(colors) , size=6),
+    )
+)
+
+app.layout = get_layout(fig)
+app.run_server()
