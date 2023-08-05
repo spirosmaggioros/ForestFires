dd97acd0bb37e794bb3aabdf5381a77c7800444b
Spiros Maggioros <spirosastro@gmail.com>
2023-08-05 12:41:56 +0300

    visualization changes

--- main.py
+++ main.py
@@ -7,32 +7,54 @@ import plotly.graph_objects as go
 import plotly.express as px
 from visualization.visual import get_layout
 from sklearn.datasets import load_digits
-from dash import Dash
+from dash import Dash ,  Output, Input
 from sklearn import metrics
 from utils.algorithms import KNN_algorithm, dbscan_algorithm, svm_algorithm, kmeans_algorithm, random_forest_algorithm , KNNRegressor
 from utils.data_preprocess import process_data_for_clustering
 
 app = Dash(__name__, suppress_callback_exceptions=True)
 
-data = pd.read_csv("forestfires.csv")
+dropdown_figures = ['Barplot' , 'Scatterplot' , 'Lineplot']
 
+def fill_data(meteorological_data , predictions):
+    meteorological_data['FFMC'] = predictions[: , 0]
+    meteorological_data['DMC'] = predictions[: , 1]
+    meteorological_data['DC'] = predictions[: , 2]
+    meteorological_data['ISI'] = predictions[: , 3]
+    return meteorological_data
+
+
+data = pd.read_csv("forestfires.csv")
+    
 data = process_data_for_clustering(data)
 meteorological_data = pd.read_csv('naxos_data.csv')
+
 knn = KNNRegressor(data)
 predictions = knn.predict(meteorological_data[['temp' , 'RH' , 'wind' , 'rain']].values.tolist())
-meteorological_data['FFMC'] = predictions[: , 0]
-meteorological_data['DMC'] = predictions[: , 1]
-meteorological_data['DC'] = predictions[: , 2]
-meteorological_data['ISI'] = predictions[: , 3]
-
+meteorological_data = fill_data(meteorological_data , predictions)
 meteorological_data = process_data_for_clustering(meteorological_data, include_area = False)
 
-#y_pred, y_test = KNN_algorithm(data , to_predict='area')
-#print(metrics.confusion_matrix(y_test, y_pred))
-#print(metrics.accuracy_score(y_test, y_pred))
+fig = px.bar(meteorological_data , x=meteorological_data['Date'].tolist() ,y="danger" , title="Danger level")
+
+
+@app.callback(
+    Output('graph1' , 'figure' , allow_duplicate=True),
+    Input('dropdown-figure' , 'value'),
+    prevent_initial_call=True
+)
+def update_data(figure_value):
+    global meteorological_data
+    if figure_value == "Barplot": 
+        fig = px.bar(meteorological_data , x=meteorological_data['Date'].tolist(), y="danger", title="Danger level for: " )
+    elif figure_value == "Scatterplot":
+        fig = px.scatter(meteorological_data , x="Date" , y="temp" ,color='danger')
+    else:
+        fig = px.line(meteorological_data, x="Date" , y="danger")
+    return fig
+
+
 
 colors = {1:'blue' , 2:'yellow' , 3:'red' , 4:'darkred'}
 
-fig = px.line(meteorological_data , x="Date" , y="danger" , title="danger levels over the next 5 days")
-app.layout = get_layout(fig)
-app.run_server()
+app.layout = get_layout(fig ,dropdown_figures)
+app.run_server(threaded=True)
--- visualization/visual.py
+++ visualization/visual.py
@@ -2,16 +2,50 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 from dash import html, dcc, dash_table
 
-def get_layout(fig):
+def get_layout(fig , dropdown_figure):
     layout = html.Div(
         children=[
-        html.H1(children="Visualization Tool for the project of Forest Fire Danger Detection"),
+        dbc.Row([
+            dbc.Col(
+                dbc.Card([
+                    dbc.CardBody([
+                        html.H2(children="Prediction Model for Forest Fires Danger", style={"textAlign": "center"}, ),
+                        html.H5(children="Data Analysis Platform",
+                                style={"textAlign": "center", "fontStyle": "italic"}, ),
+                    ]),
+                    dbc.CardImg(src='/Users/spirosmag/Documents/machinevision/ForestFires/visualization/fires.png', top=False, ),
+                ],
+                    color="info", inverse=True
+                ),
+                width={"size": 3},
+            ),
+        ],
+            align="left",
+            justify="left",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody([
+                            dcc.Markdown(children="Select Case" , style={'font-size': 20}),
+                            html.Br(),
+
+                            dcc.Dropdown(dropdown_figure, 'dbscan', id='dropdown-figure',  style={'font-size': 20}),
+                            html.Br(),
+
+                        ]),
+                    ),
+                    width={"size": 3},
+                ),],),
         dbc.Row(
             dbc.Col(
-                    dcc.Graph(id='graph', figure=fig, style={'width': '90vh', 'height': '90vh'}),
+                    dcc.Graph(id='graph1', figure=fig, style={'width': '90vh', 'height': '90vh'}),
                 align = "center",
-            ),
+            ), 
         ),],
+        
+        
     )
 
     return layout
