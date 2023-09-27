cf0da1b90de47dddce1495e0ecce0ca74a9f5bca
Spiros Maggioros <51701672+spirosmaggioros@users.noreply.github.com>
2023-09-27 11:48:19 +0300

    Update algorithms.py

--- utils/algorithms.py
+++ utils/algorithms.py
@@ -11,71 +11,106 @@ from sklearn import metrics
 from sklearn.metrics import accuracy_score , confusion_matrix
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
 import math
 import pandas as pd
 from sklearn.svm import SVR
 import requests
-#import tensorflow as tf
 import matplotlib.pyplot as plt
-#from tensorflow.keras import Model
-#from tensorflow.keras import Sequential
-#from tensorflow.keras.optimizers import Adam
-#from sklearn.preprocessing import StandardScaler
-#from tensorflow.keras.layers import Dense, Dropout
-#from tensorflow.keras.losses import MeanSquaredLogarithmicError
+from sklearn.preprocessing import StandardScaler
 
-def random_forest_algorithm(df):
+def random_forest_algorithm(df , predict):
     #print(df.head())
     #spliting the dataset
     scaler = StandardScaler()
-    X = df[['temp' ,'RH', 'wind' , 'rain', 'duration']].values.tolist()
+    X = df[['temp' ,'RH', 'wind' , 'rain']].values.tolist()
     X = scaler.fit_transform(X)
-    y = df[['area']].values.tolist()
+    y = df[[predict]].values.tolist()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+    model = RandomForestRegressor(n_estimators=490 , random_state=42)
+    model.fit(X_train , np.ravel(y_train))
+    y_pred = model.predict(X_test)
+    accuracy = mean_absolute_error(y_test , np.ravel(y_pred))
+    print("Accuracy is " + str(accuracy))
 
     # Store the accuracies for different estimator values
-    accuracies = []
-    estimators = np.arange(10 ,500 , 10)
-     #Train and evaluate the model for each estimator value
-    for n_estimators in estimators:
-        print(n_estimators)
-        model = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
-        model.fit(X_train, np.ravel(y_train))
-        y_pred = model.predict(X_test)
-        accuracy = mean_absolute_error(y_test , np.ravel(y_pred)) 
-        accuracies.append(accuracy)
+    #ccuracies = []
+    #estimators = np.arange(10 ,1000 , 10)
+    #Train and evaluate the model for each estimator value
+    #for n_estimators in estimators:
+    #    print(n_estimators)
+    #    model = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
+    #    model.fit(X_train, np.ravel(y_train))
+    #    y_pred = model.predict(X_test)
+    #    accuracy = mean_absolute_error(y_test , np.ravel(y_pred)) 
+    #    accuracies.append(accuracy)
 
      #Plot the results
-    plt.plot(estimators, accuracies, marker='o')
-    plt.xlabel("Estimator Values")
-    plt.ylabel("Accuracy")
-    plt.legend()
-    plt.title("Best parameters for the RFC")
-    plt.show()
+    #plt.plot(estimators, accuracies, marker='o')
+    #plt.xlabel("Estimator Values")
+    #plt.ylabel("Accuracy")
+    #plt.legend()
+    #plt.title("Best parameters for the RFC")
+    #plt.show()
     #using the best model and graph it
-    rf = RandomForestRegressor(n_estimators=3, random_state=42)
-    rf.fit(X_train, np.ravel(y_train))
-    y_pred = rf.predict(X_test)
+    #rf = RandomForestRegressor(n_estimators=3, random_state=42)
+    #rf.fit(X_train, np.ravel(y_train))
+    #y_pred = rf.predict(X_test)
     plt.plot(y_test , label="testing")
     plt.plot(y_pred , label="predictions")
     plt.legend()
     plt.show()
-    print(mean_absolute_error(y_test , y_pred))
-    return rf 
+    #print(mean_absolute_error(y_test , y_pred))
+    return model
 
 def KNNRegressor(data, predicted_value = 'FFMC'):
     X = data[['temp' , 'RH' , 'wind' , 'rain']].values.tolist()
-    y = data[['FFMC' , 'DMC' , 'DC', 'ISI']].values.tolist()
-    
-    X_train , X_test , y_train , y_test = train_test_split(X , y , test_size=0.2 , random_state=42)
-    
-    knn = KNeighborsRegressor(n_neighbors=12)
+    y = data[[predicted_value]].values.tolist()
+    param = 1
+    if predicted_value == 'FFMC':
+        param = 44
+    elif predicted_value == 'DMC' or predicted_value == 'DC':
+        param = 2
+    else:
+        param = 3
+    knn = KNeighborsRegressor(n_neighbors=param , n_jobs=-1)
     knn.fit(X , y)
+    #knn = KNeighborsRegressor(n_neighbors=5)
+    #knn.fit(X_train , y_train)
+    #preds = knn.predict(X_test)
+    #plt.plot(preds)
+    #plt.plot(y_test)
+    #plt.show()
     return knn
-    
+
+def nn(data , to_predict): 
+    X = data[['temp' , 'RH' , 'wind' , 'rain']].values.tolist()
+    y = data[to_predict].values.tolist()
+    X_train , X_test , y_train , y_test = train_test_split(X , y , test_size=0.2 , random_state=42)
+    model = keras.Sequential([
+        layers.Dense(units=4, activation='relu' , input_shape=[4]),
+        layers.Dense(units=4, activation='relu'),
+        layers.Dense(units=4 , activation='relu'),
+        layers.Dense(units=4 , activation='relu'),
+        layers.Dense(units=1)
+        ])
+
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+        )
+
+    history = model.fit(
+        X_train , y_train,
+        validation_data = (X_test , y_test),
+        batch_size = 32,
+        epochs=100,
+        )
+
+    history_df = pd.DataFrame(history.history)
+    history_df['loss'].plot()
+    plt.show()
+    return model
 
 def svm_algorithm(data , kernel='linear', random_state=42):
     #print(data.head())
@@ -126,4 +161,5 @@ def agglomerative_clustering(data , n_clusters = 5 , affinity='euclidean' , link
     plt.scatter(X[labels==3, 0], X[labels==3, 1], marker='o', color='purple')
     plt.scatter(X[labels==4, 0], X[labels==4, 1],  marker='o', color='orange')
     plt.show()
+
 
